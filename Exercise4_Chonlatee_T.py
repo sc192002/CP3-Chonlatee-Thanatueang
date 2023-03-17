16696ab2fe63c98ae396996e147f11bdb2d2b98b
sc192002 <sc192002@hotmail.com>
2023-03-17 13:24:39 +0700

    https://github.com/sc192002/CP3-Chonlatee-Thanatueang.git
    Chonlatee Thanatueang ได้ส่งการบ้าน Exercise 4 เรียบร้อยแล้วที่ URL : https://github.com/sc192002/CP3-Chonlatee-Thanatueang.git

---
+++ Exercise4_Chonlatee_T.py
@@ -0,0 +1,13 @@
+print("--- Your Score ---")
+fe = ("Foundation English :")
+fes = 3.1
+gb = ("General Business :")
+gbs = 3.0
+ics = ("Introduction to Computer Systems :")
+icss = 2.8
+cp = ("Computer Programming :")
+cps = 3.0
+print(fe,fes)
+print(gb,gbs)
+print(ics,icss)
+print(cp,cps)
